23e3994ace42ee40912b44e7f5b9c378e6a2fb48
kristi <kristimihali3@gmail.com>
2020-12-01 12:24:41 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ res.sendFile(path.join(__dirname+'/dist/tp3-web-avancee/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
